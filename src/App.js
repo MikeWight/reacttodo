3620d46cfe768a30de9a9fbeddd22095fd1ef77d
MikeWight <michel.wight@hotmail.com>
2022-10-08 11:41:48 -0300

    completed crud todo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AiOutlinePlus } from 'react-icons/ai';
 import Todo from './Todo';
+import { db } from './firebase';
+import {
+    query,
+    collection,
+    onSnapshot,
+    updateDoc,
+    doc,
+    addDoc,
+    deleteDoc,
+} from 'firebase/firestore';
 
 const style = {
     bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1cb5e0]`,
@@ -13,22 +23,56 @@ const style = {
 };
 
 function App() {
-    const [todos, setTodos] = useState(['Learn React', 'Grind Leetcode']);
+    const [todos, setTodos] = useState([]);
+    const [text, setText] = useState('');
 
     // Create todo
+    const createTodo = async (e) => {
+        e.preventDefault();
+        if (text === '') {
+            alert('Fill the field please');
+            return;
+        }
+        await addDoc(collection(db, 'todos'), {
+            text: text,
+            completed: false,
+        });
+        setText('');
+    };
 
     // Read todo
+    useEffect(() => {
+        const q = query(collection(db, 'todos'));
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            let todosArr = [];
+            querySnapshot.forEach((doc) => {
+                todosArr.push({ ...doc.data(), id: doc.id });
+            });
+            setTodos(todosArr);
+        });
+        return () => unsubscribe();
+    }, []);
 
     // Update todo
+    const toggleComplete = async (todo) => {
+        await updateDoc(doc(db, 'todos', todo.id), {
+            completed: !todo.completed,
+        });
+    };
 
     // Delete Todo
+    const deleteTodo = async (id) => {
+        await deleteDoc(doc(db, 'todos', id));
+    };
 
     return (
         <div className={style.bg}>
             <div className={style.container}>
                 <h3 className={style.heading}>Todo App</h3>
-                <form className={style.form}>
+                <form onSubmit={createTodo} className={style.form}>
                     <input
+                        value={text}
+                        onChange={(e) => setText(e.target.value)}
                         className={style.input}
                         type="text"
                         placeholder="Add Todo"
@@ -39,10 +83,19 @@ function App() {
                 </form>
                 <ul>
                     {todos.map((todo, index) => (
-                        <Todo key={index} todo={todo} />
+                        <Todo
+                            key={index}
+                            todo={todo}
+                            toggleComplete={toggleComplete}
+                            deleteTodo={deleteTodo}
+                        />
                     ))}
                 </ul>
-                <p className={style.count}>You have 2 todos</p>
+                {todos.length < 1 ? null : (
+                    <p
+                        className={style.count}
+                    >{`You have ${todos.length} todos`}</p>
+                )}
             </div>
         </div>
     );
